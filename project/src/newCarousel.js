017ee39558bce92eecaf7a2af2f8ff98c7d629a0
Alexander Gorham <agram@hotmail.co.uk>
2023-12-13 04:17:26 +0100

    Working auto progression, circles added

--- project/src/newCarousel.js
+++ project/src/newCarousel.js
@@ -15,16 +15,37 @@ export default function newCarousel()
     function autoSwitch()
     {
         const slides = Array.from(document.querySelectorAll(".slide"));
+        const circles = Array.from(document.querySelectorAll(".circle"));
+
+        for (let i = 0; i < slides.length; i+=1)
+        {
+            slides[i].id = (`img-${  i.toString()}`);
+            circles[i].id = (`circle-${  i.toString()}`);
+        }
+
+
         const activeSlide = slides.find(slide => slide.dataset.active !== undefined);
         let newIndex = [...slides].indexOf(activeSlide) + 1;
+
+        
+
+        const activeCircle = circles.find(circle => circle.dataset.active !== undefined);
+        let newCircleIndex = [...circles].indexOf(activeCircle) + 1;
+
 
         if (newIndex >= slides.length)
         {
             newIndex = 0;
+            newCircleIndex = 0;
+
         }
+
 
         slides[newIndex].dataset.active = true;
         delete activeSlide.dataset.active;
+
+        circles[newCircleIndex].dataset.active = true;
+        delete activeCircle.dataset.active;
 
         myStopFunction();
         myTimeout = setTimeout(autoSwitch, 5000);
