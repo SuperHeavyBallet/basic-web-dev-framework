f9fc780caea673749f1ae9fe21dcec57a7b7cc03
Alexander Gorham <agram@hotmail.co.uk>
2023-12-26 00:40:00 +0100

    Setup clean work environment

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# top-js-widgets
-Drop Down Menu, Carousel etc
+Basic Template to use for Web Dev
+
+Includes:
+Webpack
+Babel
+
+Possible error not being able to find fs/promise on build
+Solution seems to be to manually update npm version to latest
--- project/webpack.config.js
+++ project/webpack.config.js
@@ -6,4 +6,20 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Apply this rule to .js files
+                exclude: /node_modules/, // Don't apply to files residing in node_modules
+                use: {
+                    loader: "babel-loader", // Use Babel loader
+                    options: {
+                        presets: ["@babel/preset-env"] // Use @babel/preset-env for transpilation
+                    }
+                }
+            }
+            // Add more rules for handling CSS, images, etc. if needed
+        ]
+    }
 };
+
